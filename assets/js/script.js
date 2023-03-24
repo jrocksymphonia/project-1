30018721c9e2c68fa64627f47bae35eefa39b0b5
ashleytrollinger <ashley@trollingers.com>
2023-03-23 20:00:35 -0400

    if taken out

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var apiKey = "634d2baea68e8fb7f9f95bd75f1f9406";
 //The value is the activity of choice! 
 var artistTag = "cleaning";
 var albumTag = "exercise";
-var trackTag= "";
+var trackTag = "";
 
 //Query parameters for our form. Currently set up to look up a specific artist/album/track
 var albumResult = "?method=tag.gettopalbums&tag=" + albumTag + "&limit=1&api_key=" + apiKey + "&format=json";
@@ -29,7 +29,7 @@ function loadForm() {
 
 //generate activity via Bored API
 function getActivity() {
-    
+
     fetch(randomActivity)
         .then(function (response) {
             //We need to break apart the object and randomize it
@@ -41,7 +41,7 @@ function getActivity() {
             var newOption = document.createElement('option');
             newOption.textContent = data.activity;
             dropdown.appendChild(newOption);
-            
+
         })
 
 }
@@ -58,57 +58,57 @@ function loadResults() {
     form.remove();
     //Getting the answer to question one to determine which fetch to use
     var questionOneAnswers = document.getElementsByName('question1');
-    for (i=0;i< questionOneAnswers.length; i++){
-        if(questionOneAnswers[i].checked){
+    for (i = 0; i < questionOneAnswers.length; i++) {
+        if (questionOneAnswers[i].checked) {
             var questionOneAnswer = questionOneAnswers[i];
         }
     }
-    
+
     //Maybe create 3 seperate functions to call inside each of these instead of moving the whole fetch. That way we can also use the users second answer to filter results
-    if(questionOneAnswer == "Artist"){
-        //Artist fetch
-    }else if (questionOneAnswer == "Track"){
-        // Track fetch
-    }else {
-        //Album fetch
-    }
+    // if(questionOneAnswer == "Artist"){
+    //Artist fetch
+    // }else if (questionOneAnswer == "Track"){
+    // Track fetch
+    // }else {
+    //Album fetch
+    // }
 }
 
 //last.fm fetch tester (ablum)
 fetch(lastFmBaseURL + albumResult)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    // var newOption = document.createElement('option');
-    // newOption.textContent = data.activity;
-    // dropdown.appendChild(newOption);
-    
-})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        // var newOption = document.createElement('option');
+        // newOption.textContent = data.activity;
+        // dropdown.appendChild(newOption);
+
+    })
 
 //last.fm fetch tester (song)
 fetch(lastFmBaseURL + trackResult)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    // var newOption = document.createElement('option');
-    // newOption.textContent = data.activity;
-    // dropdown.appendChild(newOption);
-    
-})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        // var newOption = document.createElement('option');
+        // newOption.textContent = data.activity;
+        // dropdown.appendChild(newOption);
+
+    })
 
 //last.fm fetch tester (artist)
 fetch(lastFmBaseURL + artistResult)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    // var newOption = document.createElement('option');
-    // newOption.textContent = data.activity;
-    // dropdown.appendChild(newOption);
-    
-})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        // var newOption = document.createElement('option');
+        // newOption.textContent = data.activity;
+        // dropdown.appendChild(newOption);
+
+    })
